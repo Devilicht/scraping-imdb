849e1f9759094890754421d55cc8676c40b1bdc2
Oblivion <oblivionccyber@gmail.com>
2023-04-14 17:11:13 -0300

    change lib scrapy to bs4

---
+++ scraping-imdb-movies.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+from requests import get
+import os
+import pymongo
+
+
+userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0'
+headers = {'User-Agent': userAgent}
+
+url = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
+
+response = get(url,headers=headers)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+ranks = soup.select('.ratingColumn.imdbRating')
+titlesMovies = soup.select('.titleColumn')
+
+movies = [{'id': i + 1,
+'name': title.select_one('a').get_text(strip=True),
+'years': title.select_one('span').get_text(strip=True),
+'rank': ranks[i].select_one('strong').get_text(strip=True)}
+for i, title in enumerate(titlesMovies)]
+
+print (movies)
